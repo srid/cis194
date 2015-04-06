19072b420bad4320d0da56d59335698835bd4516
Sridhar Ratnakumar <srid@srid.ca>
2015-04-06 00:05:46 -0700

    towers of hanoi

--- homework1.hs
+++ homework1.hs
@@ -1,6 +1,8 @@
 module Homework1 where
 import Data.Char
 
+-- Validating Credit Card Numbers1
+
 toDigits :: Integer -> [Integer]
 toDigits n
     | n <= 0    = []
@@ -27,3 +29,14 @@ sumDigits (x:xs) = sumDigitsSolo x + sumDigits xs
 
 validate :: Integer -> Bool
 validate = (==0) . (`rem` 10) . sumDigits . doubleEveryOther . toDigits
+
+
+-- The Towers of Hanoi
+
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi n a b c
+    | n <= 0    = []
+    | n == 1    = [(a, b)]
+    | otherwise = hanoi (n-1) a c b ++ [(a, b)] ++ hanoi (n-1) c b a
