d7419e54828f7f1c732ab769eae4c5e88965a383
Sridhar Ratnakumar <srid@srid.ca>
2015-04-10 14:32:04 -0700

    remove unnecessary qualifier

--- homework3/Golf.hs
+++ homework3/Golf.hs
@@ -17,7 +17,7 @@ histogram :: [Integer] -> String
 histogram = unlines . reverse . transpose . (f =<< g) . fill 0 . freq
     where f m [] = []
           f m ((a,b):xs) = (show a ++ "=" ++ replicate b '*' ++ replicate (m - b) ' '):f m xs
-          g x = snd $ Data.List.maximumBy (comparing snd) x
+          g x = snd $ maximumBy (comparing snd) x
           fill i []     = map (\j -> (j, 0)) [i..9]
           fill i x@((a,b):xs) = if i < a then (i, 0):fill (i+1) x else (a, b):fill (i+1) xs
           freq a = [(head x, length x) | x <- group $ sort a]
