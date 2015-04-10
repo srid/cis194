97798c01fcb2ce8e79f67a14258174d777dec986
Sridhar Ratnakumar <srid@srid.ca>
2015-04-10 00:28:30 -0700

    oops

--- homework3/Golf.hs
+++ homework3/Golf.hs
@@ -2,6 +2,6 @@ module Golf where
 import Data.List (repeat, zip)
 
 skips :: [a] -> [[a]]
-skips xs = foldr f [] <$ xs (zip [1..] xs)
+skips xs = foldr f ([] <$ xs) (zip [1..] xs)
     where f (n, e) acc = map g (zip [1..] acc) where
               g (i, x) = if mod n i == 0 then e:x else x
