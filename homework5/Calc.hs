53d0973de117cf6314374b8f2fdb0dcae46294cc
Sridhar Ratnakumar <srid@srid.ca>
2015-04-12 00:16:27 -0700

    exercise 4

--- homework5/Calc.hs
+++ homework5/Calc.hs
@@ -25,3 +25,38 @@ instance Expr ExprT where
 
 reify :: ExprT -> ExprT
 reify = id              
+
+instance Expr Integer where
+    lit = toInteger
+    add = (+)
+    mul = (*)
+
+instance Expr Bool where
+    lit = (>0)
+    add = (||)
+    mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+    
+instance Expr MinMax where
+    lit = MinMax . toInteger
+    add (MinMax x) (MinMax y) = MinMax (max x y)
+    mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)                     
+
+toMod7 :: Integer -> Mod7
+toMod7 = Mod7 . toInteger . (`mod` 7)
+    
+instance Expr Mod7 where
+    lit = toMod7 . toInteger
+    add (Mod7 x) (Mod7 y) = toMod7 $ x+y
+    mul (Mod7 x) (Mod7 y) = toMod7 $ x*y
+    
+    
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+testInteger = testExp :: Maybe Integer    
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
