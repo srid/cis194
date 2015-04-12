14ed93c763fb5ca5ed9e37e626c786ade1d42ffd
Sridhar Ratnakumar <srid@srid.ca>
2015-04-12 14:26:20 -0700

    complete exercise 6

---
+++ homework5/CalcEx6.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE FlexibleInstances #-}
+module CalcEx6 where
+import qualified Data.Map as M
+    
+data VarExprT = Lit Integer
+           | Add VarExprT VarExprT
+           | Mul VarExprT VarExprT
+           | Var String
+  deriving (Show, Eq)
+
+class Expr a where
+    lit      :: Integer -> a
+    add, mul :: a -> a -> a
+           
+class (Expr a) => HasVars a where
+    var :: String -> a
+
+instance Expr VarExprT where
+    lit = Lit . toInteger
+    add = Add
+    mul = Mul
+
+instance HasVars VarExprT where                       
+    var = Var
+
+-- Data.Map.lookup applied to String to Integer map          
+type LookupVar = M.Map String Integer -> Maybe Integer
+
+instance HasVars LookupVar where
+    var = M.lookup
+
+instance Expr LookupVar where
+    lit       = const . Just
+    add x y m = case (x m, y m) of
+                  (Nothing, _) -> Nothing
+                  (_, Nothing) -> Nothing
+                  (Just a, Just b) -> Just (a + b)
+    mul x y m = case (x m, y m) of
+                  (Nothing, _) -> Nothing
+                  (_, Nothing) -> Nothing
+                  (Just a, Just b) -> Just (a * b)
+
+withVars :: [(String, Integer)]
+         -> (M.Map String Integer -> Maybe Integer)
+         -> Maybe Integer
+withVars vs exp = exp $ M.fromList vs                                                         
