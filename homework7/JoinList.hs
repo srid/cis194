4105e206270299eb2020587bea551b5c7c907c0e
Sridhar Ratnakumar <srid@srid.ca>
2015-04-13 17:57:17 -0700

    implement indexJ (untested)

--- homework7/JoinList.hs
+++ homework7/JoinList.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module JoinList where
-import Data.Monoid ((<>))    
+import Data.Monoid ((<>))
+import Sized
 
 data JoinList m a = Empty
                   | Single m a
@@ -15,3 +17,22 @@ tag :: Monoid m => JoinList m a -> m
 tag (Empty) = mempty
 tag (Single m _) = m
 tag (Append m _ _) = m
+
+
+indexJ :: (Sized b, Monoid b) =>
+          Int -> JoinList b a -> Maybe a
+indexJ i (Single m a)
+    | inBounds i m   = Nothing
+    | otherwise      = Just a
+indexJ i (Append m jl1 jl2)
+    | inBounds i m   = Nothing
+    | otherwise      = let x = tag jl1 in
+                       case x of _
+                                     | inBounds i x -> indexJ i jl1
+                                     | otherwise    -> indexJ (i - (getSize $ size x)) jl2
+indexJ _ _ = Nothing
+
+
+inBounds :: (Sized a) =>
+            Int -> a -> Bool
+inBounds i sz = i <= (getSize $ size sz)
